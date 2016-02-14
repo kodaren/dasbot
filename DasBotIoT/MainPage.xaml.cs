3e85687ad5054c4663c962b340568279b2c9aae7
unknown <arpaho@AR00078.arete.se>
2016-02-14 09:06:53 +0100

    no message

---
+++ DasBotIoT/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Emmellsoft.IoT.Rpi.SenseHat;
+using Emmellsoft.IoT.Rpi.SenseHat.Fonts.SingleColor;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DasBotIoT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SensorRunner.Run(SensorSelector.GetSensor);
+        }
+    }
+}
---
+++ DasBotIoT/TemperatureSensor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Windows.UI;
+using Emmellsoft.IoT.Rpi.SenseHat;
+using Emmellsoft.IoT.Rpi.SenseHat.Fonts.SingleColor;
+
+namespace DasBotIoT
+{
+    public class TemperatureSensor : SenseHatUtility
+    {
+        public TemperatureSensor(ISenseHat senseHat)
+            : base(senseHat)
+        {
+        }
+
+
+        public override void Run()
+        {
+            var tinyFont = new TinyFont();
+
+            ISenseHatDisplay display = SenseHat.Display;
+
+            while (true)
+            {
+                SenseHat.Sensors.HumiditySensor.Update();
+
+                if (SenseHat.Sensors.Temperature.HasValue)
+                {
+                    int temperature = (int)Math.Round(SenseHat.Sensors.Temperature.Value);
+                    string text = temperature.ToString();
+
+                    if (text.Length > 2)
+                    {
+                        // Too long to fit the display!
+                        text = "**";
+                    }
+
+                    display.Clear();
+                    tinyFont.Write(display, text, Colors.White);
+                    display.Update();
+
+                    // Sleep quite some time; the temperature usually change quite slowly...
+                    Sleep(TimeSpan.FromSeconds(5));
+                }
+                else
+                {
+                    // Rapid update until there is a temperature reading.
+                    Sleep(TimeSpan.FromSeconds(0.5));
+                }
+            }
+        }
+
+
+    }
+}
